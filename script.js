649b40f5dce4132438faf504132959599da13f53
Juan Luis García <juanluisgarcialopez@outlook.es>
2019-02-24 13:05:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -52,12 +52,14 @@ window.onSpotifyPlayerAPIReady = () => {
 
   // Ready
   player.on('ready', data => {
+    alert("Preparado");
     console.log('Ready with Device ID', data.device_id);
     
     // Play a track using our new device ID
     play(data.device_id, "spotify:track:5q53KFQSjZnJGHlu6zsW9X");
     
     setTimeout( () => {
+      alert("voy a cambiar de canción");
       play(data.device_id, "spotify:track:5ya2gsaIhTkAuWYEMB0nw5");      
     },5000);
   });
